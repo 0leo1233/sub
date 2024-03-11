1cf879f8cb8b20440e16dd5e1309521cf404cc34
Leo Chen <leo.chen@dometic.com>
2024-03-11 10:14:21 +0800

    Initial

---
+++ sub.h
@@ -0,0 +1,9 @@
+#ifndef __SUB_H__
+#define __SUB_H__
+
+#include "sub_common.h"
+
+void print_sub(void);
+
+
+#endif //__SUB_H__
---
+++ sub_common.h
@@ -0,0 +1,8 @@
+#ifndef __SUB_COMMON_H__
+#define __SUB_COMMON_H__
+
+#include <stdio.h>
+
+
+
+#endif //__SUB_COMMON_H__
